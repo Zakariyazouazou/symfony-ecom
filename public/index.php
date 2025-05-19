3b14f752f17c4544f57262031457b2de47c03ba6
zakariya <zakariyazouazou@gmail.com>
2025-05-20 00:45:58 +0100

    upsdate

--- public/index.php
+++ public/index.php
@@ -2,19 +2,7 @@
 
 use App\Kernel;
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-header("Allow: *");
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-if ($method === "OPTIONS") {
-    die();
-}
-
-
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
